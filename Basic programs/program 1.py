be2f3b33260c37160af70fb1b0cbd15d4e094687
Mahanvitha2004 <123939689+Mahanvitha2004@users.noreply.github.com>
2023-01-30 17:30:09 +0530

    Add files via upload

---
+++ Basic programs/program 1.py
@@ -0,0 +1,6 @@
+List=[10,20,14]
+print(List)
+print(List[0])
+print(List[2])
+print(List[-1])
+print(len(List))
---
+++ Basic programs/program 2.py
@@ -0,0 +1,6 @@
+List=[2,4,6]
+List.append(2)
+List.append(4)
+List.append(6)
+print("\nList after Addition of Three elements:")
+print(List)
